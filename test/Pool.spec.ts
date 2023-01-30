98f9b1d73eb68d8d191512f7a070f4f633779443
Vanya A. Sergeev <v@sergeev.io>
2023-01-30 14:14:08 -0600

    test: add priceNote() and sellNote() tests to Pool

--- test/Pool.spec.ts
+++ test/Pool.spec.ts
@@ -9,6 +9,7 @@ import {
   TestLendingPlatform,
   TestNoteToken,
   TestNoteAdapter,
+  TestLoanReceipt,
   FixedInterestRateModel,
   AllowCollateralFilter,
   LiquidityManager,
@@ -16,6 +17,8 @@ import {
 } from "../typechain";
 
 import { extractEvent, expectEvent } from "./helpers/EventUtilities";
+import { elapseUntilTimestamp } from "./helpers/BlockchainUtilities";
+import { FixedPoint } from "./helpers/FixedPoint.ts";
 
 describe("Pool", function () {
   let accounts: SignerWithAddress[];
@@ -24,6 +27,7 @@ describe("Pool", function () {
   let lendingPlatform: TestLendingPlatform;
   let noteToken: TestNoteToken;
   let noteAdapter: TestNoteAdapter;
+  let loanReceiptLib: TestLoanReceipt;
   let liquidityManagerLib: LiquidityManager;
   let interestRateModel: FixedInterestRateModel;
   let allowCollateralFilter: AllowCollateralFilter;
@@ -41,6 +45,7 @@ describe("Pool", function () {
     const testLendingPlatformFactory = await ethers.getContractFactory("TestLendingPlatform");
     const testNoteAdapterFactory = await ethers.getContractFactory("TestNoteAdapter");
     const allowCollateralFilterFactory = await ethers.getContractFactory("AllowCollateralFilter");
+    const testLoanReceiptFactory = await ethers.getContractFactory("TestLoanReceipt");
     const liquidityManagerFactory = await ethers.getContractFactory("LiquidityManager");
     const fixedInterestRateModelFactory = await ethers.getContractFactory("FixedInterestRateModel");
 
@@ -67,6 +72,10 @@ describe("Pool", function () {
     noteAdapter = (await testNoteAdapterFactory.deploy(lendingPlatform.address)) as TestNoteAdapter;
     await noteAdapter.deployed();
 
+    /* Deploy loan receipt library */
+    loanReceiptLib = await testLoanReceiptFactory.deploy();
+    await loanReceiptLib.deployed();
+
     /* Deploy allow collateral filter */
     allowCollateralFilter = await allowCollateralFilterFactory.deploy([nft1.address]);
     await allowCollateralFilter.deployed();
@@ -76,7 +85,7 @@ describe("Pool", function () {
     await liquidityManagerLib.deployed();
 
     /* Deploy test interest rate model */
-    interestRateModel = await fixedInterestRateModelFactory.deploy(ethers.utils.parseEther("0.02"));
+    interestRateModel = await fixedInterestRateModelFactory.deploy(FixedPoint.normalizeRate("0.02"));
 
     /* Deploy pool */
     const poolFactory = await ethers.getContractFactory("Pool", {
@@ -183,7 +192,10 @@ describe("Pool", function () {
       expect(await tok1.balanceOf(accountDepositors[0].address)).to.equal(ethers.utils.parseEther("997"));
     });
     it("fails on invalid tick spacing", async function () {
-      /* FIXME */
+      await pool.connect(accountDepositors[0]).deposit(ethers.utils.parseEther("10"), ethers.utils.parseEther("1"));
+      await expect(
+        pool.connect(accountDepositors[0]).deposit(ethers.utils.parseEther("10.1"), ethers.utils.parseEther("2"))
+      ).to.be.revertedWithCustomError(liquidityManagerLib, "InsufficientTickSpacing");
     });
     it("fails on insolvent tick", async function () {
       /* FIXME */
@@ -279,7 +291,7 @@ describe("Pool", function () {
       /* Withdraw tx should have no events */
       expect((await withdrawTx.wait()).logs.length).to.equal(0);
     });
-    it("withdraws fullly available redemption", async function () {
+    it("withdraws fully available redemption", async function () {
       /* Deposit 1 ETH */
       await pool.connect(accountDepositors[0]).deposit(ethers.utils.parseEther("10"), ethers.utils.parseEther("1"));
       /* Redeem 0.5 shares */
@@ -321,6 +333,10 @@ describe("Pool", function () {
     });
   });
 
+  /****************************************************************************/
+  /* Helper functions */
+  /****************************************************************************/
+
   async function setupLiquidity(): Promise<void> {
     const NUM_TICKS = 16;
     const TICK_SPACING_BASIS_POINTS = await liquidityManagerLib.TICK_SPACING_BASIS_POINTS();
@@ -332,38 +348,172 @@ describe("Pool", function () {
     }
   }
 
+  async function createActiveLoan(
+    principal: ethers.BigNumber,
+    repayment: ethers.BigNumber,
+    duration?: number
+  ): Promise<ethers.BigNumber> {
+    const lendTx = await lendingPlatform
+      .connect(accountLender)
+      .lend(accountBorrower.address, nft1.address, 123, principal, repayment, duration || 30 * 86400);
+    return (await extractEvent(lendTx, lendingPlatform, "LoanCreated")).args.loanId;
+  }
+
+  async function createExpiredLoan(
+    principal: ethers.BigNumber,
+    repayment: ethers.BigNumber
+  ): Promise<ethers.BigNumber> {
+    /* Create active loan */
+    const loanId = await createActiveLoan(principal, repayment);
+
+    /* Wait for loan expiration */
+    const startTime = (await lendingPlatform.loans(loanId)).startTime.toNumber();
+    const duration = (await lendingPlatform.loans(loanId)).duration;
+    await elapseUntilTimestamp(startTime + duration + 1);
+
+    return loanId;
+  }
+
+  async function createRepaidLoan(principal: ethers.BigNumber, repayment: ethers.BigNumber): Promise<ethers.BigNumber> {
+    /* Create active loan */
+    const loanId = await createActiveLoan(principal, repayment);
+
+    /* Repay */
+    await lendingPlatform.connect(accountBorrower).repay(loanId);
+
+    return loanId;
+  }
+
+  async function createLiquidatedLoan(
+    principal: ethers.BigNumber,
+    repayment: ethers.BigNumber
+  ): Promise<ethers.BigNumber> {
+    /* Create expired loan */
+    const loanId = await createExpiredLoan(principal, repayment);
+
+    /* Liquidate */
+    await lendingPlatform.connect(accountLender).liquidate(loanId);
+
+    return loanId;
+  }
+
+  /****************************************************************************/
+  /* Note Tests */
+  /****************************************************************************/
+
   describe("#priceNote", async function () {
     beforeEach("setup liquidity", async function () {
       await setupLiquidity();
     });
     it("prices note", async function () {
-      const principal = ethers.utils.parseEther("25");
-      const repayment = principal.mul(10200).div(10000);
+      /* Create loan */
+      const loanId = await createActiveLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+
+      /* Price note */
+      const purchasePrice = await pool.priceNote(noteToken.address, loanId, []);
+      expect(purchasePrice.sub(ethers.utils.parseEther("25.95733041")).abs()).to.be.lt(
+        ethers.utils.parseEther("0.0000001")
+      );
+    });
+    it("fails on insufficient liquidity", async function () {
+      const loanId = await createActiveLoan(ethers.utils.parseEther("30"), ethers.utils.parseEther("31"));
+      await expect(pool.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        liquidityManagerLib,
+        "InsufficientLiquidity"
+      );
+    });
+    it("fails on invalid loan status (repaid)", async function () {
+      const loanId = await createRepaidLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+      await expect(pool.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        pool,
+        "InvalidLoanStatus"
+      );
+    });
+    it("fails on invalid loan status (expired)", async function () {
+      const loanId = await createExpiredLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+      await expect(pool.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        pool,
+        "InvalidLoanStatus"
+      );
+    });
+    it("fails on invalid loan status (liquidated)", async function () {
+      const loanId = await createLiquidatedLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+      await expect(pool.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        pool,
+        "InvalidLoanStatus"
+      );
+    });
+    it("fails on unsupported loan duration", async function () {
+      const loanId = await createActiveLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"), 35 * 86400);
+      await expect(pool.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        pool,
+        "UnsupportedLoanDuration"
+      );
+    });
+    it("fails on unsupported collateral", async function () {
+      /* Create NFT 2 */
+      const testERC721Factory = await ethers.getContractFactory("TestERC721");
+      const nft2 = (await testERC721Factory.deploy("NFT 2", "NFT2", "https://nft2.com/token/")) as TestERC721;
+      await nft2.deployed();
+
+      /* Mint NFT 2 to borrower */
+      await nft2.mint(accountBorrower.address, 123);
+      /* Approve lending platform to transfer NFT */
+      await nft2.connect(accountBorrower).setApprovalForAll(lendingPlatform.address, true);
 
       /* Create loan */
       const lendTx = await lendingPlatform
         .connect(accountLender)
-        .lend(accountBorrower.address, nft1.address, 123, principal, repayment, 30 * 86400);
+        .lend(
+          accountBorrower.address,
+          nft2.address,
+          123,
+          ethers.utils.parseEther("25"),
+          ethers.utils.parseEther("26"),
+          30 * 86400
+        );
       const loanId = (await extractEvent(lendTx, lendingPlatform, "LoanCreated")).args.loanId;
 
-      /* Price note */
-      const purchasePrice = await pool.priceNote(noteToken.address, loanId, []);
-      expect(purchasePrice.sub(ethers.utils.parseEther("25.5")).abs()).to.be.lt(ethers.utils.parseEther("0.0000001"));
-    });
-    it("fails on insufficient liquidity", async function () {
-      /* FIXME */
-    });
-    it("fails on unsupported collateral", async function () {
-      /* FIXME */
-    });
-    it("fails on invalid loan status", async function () {
-      /* FIXME */
+      await expect(pool.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        pool,
+        "UnsupportedCollateral",
+        0
+      );
     });
     it("fails on unsupported currency token", async function () {
-      /* FIXME */
-    });
-    it("fails on unsupported loan duration", async function () {
-      /* FIXME */
+      /* Create Token 2 */
+      const testERC20Factory = await ethers.getContractFactory("TestERC20");
+      const tok2 = (await testERC20Factory.deploy(
+        "Token 2",
+        "TOK2",
+        18,
+        ethers.utils.parseEther("10000")
+      )) as TestERC20;
+      await tok2.deployed();
+
+      /* Deploy second pool */
+      const poolFactory = await ethers.getContractFactory("Pool", {
+        libraries: { LiquidityManager: liquidityManagerLib.address },
+      });
+      const pool2 = await poolFactory.deploy(
+        tok2.address,
+        30 * 86400,
+        allowCollateralFilter.address,
+        interestRateModel.address,
+        ethers.constants.AddressZero
+      );
+      await pool2.deployed();
+
+      /* Add note token to pool */
+      await pool2.setLoanAdapter(noteToken.address, noteAdapter.address);
+
+      /* Create loan */
+      const loanId = await createActiveLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+
+      await expect(pool2.priceNote(noteToken.address, loanId, [])).to.be.revertedWithCustomError(
+        pool,
+        "UnsupportedCurrencyToken"
+      );
     });
   });
 
@@ -372,34 +522,154 @@ describe("Pool", function () {
       await setupLiquidity();
     });
     it("sells note", async function () {
-      const principal = ethers.utils.parseEther("25");
-      const repayment = principal.mul(10200).div(10000);
+      /* Create loan */
+      const loanId = await createActiveLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+
+      /* Sell note */
+      const sellNoteTx = await pool
+        .connect(accountLender)
+        .sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), []);
+      await expectEvent(sellNoteTx, noteToken, "Transfer", {
+        from: accountLender.address,
+        to: pool.address,
+        tokenId: loanId,
+      });
+      await expectEvent(sellNoteTx, tok1, "Transfer", {
+        from: pool.address,
+        to: accountLender.address,
+      });
+      await expect(sellNoteTx).to.emit(pool, "LoanPurchased");
+
+      /* Extract purchase price */
+      const purchasePrice = (await extractEvent(sellNoteTx, tok1, "Transfer")).args.value;
+
+      /* Extract loan receipt */
+      const loanReceiptHash = (await extractEvent(sellNoteTx, pool, "LoanPurchased")).args.loanReceiptHash;
+      const loanReceipt = (await extractEvent(sellNoteTx, pool, "LoanPurchased")).args.loanReceipt;
+
+      /* Validate hash */
+      expect(loanReceiptHash).to.equal(await loanReceiptLib.hash(loanReceipt));
+
+      /* Validate loan receipt */
+      const decodedLoanReceipt = await loanReceiptLib.decode(loanReceipt);
+      expect(decodedLoanReceipt.version).to.equal(1);
+      expect(decodedLoanReceipt.platform).to.equal(noteToken.address);
+      expect(decodedLoanReceipt.loanId).to.equal(loanId);
+      expect(decodedLoanReceipt.borrower).to.equal(accountBorrower.address);
+      expect(decodedLoanReceipt.maturity).to.equal(
+        (await lendingPlatform.loans(loanId)).startTime.toNumber() + 30 * 86400
+      );
+      expect(decodedLoanReceipt.duration).to.equal(30 * 86400);
+      expect(decodedLoanReceipt.collateralToken).to.equal(nft1.address);
+      expect(decodedLoanReceipt.collateralTokenId).to.equal(123);
+      expect(decodedLoanReceipt.nodeReceipts.length).to.equal(16);
+
+      /* Sum used and pending totals from node receipts */
+      let totalUsed = ethers.constants.Zero;
+      let totalPending = ethers.constants.Zero;
+      for (const nodeReceipt of decodedLoanReceipt.nodeReceipts) {
+        totalUsed = totalUsed.add(nodeReceipt.used);
+        totalPending = totalPending.add(nodeReceipt.pending);
+      }
+
+      /* Validate used and pending totals */
+      expect(totalUsed).to.equal(purchasePrice);
+      expect(totalPending).to.equal(ethers.utils.parseEther("26"));
+
+      /* Validate loan state */
+      expect(await pool.loans(loanReceiptHash)).to.equal(1);
+    });
+    it("fails on insufficient liquidity", async function () {
+      const loanId = await createActiveLoan(ethers.utils.parseEther("30"), ethers.utils.parseEther("31"));
+      await expect(
+        pool.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(liquidityManagerLib, "InsufficientLiquidity");
+    });
+    it("fails on invalid loan status (repaid)", async function () {
+      const loanId = await createRepaidLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+      await expect(
+        pool.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(pool, "InvalidLoanStatus");
+    });
+    it("fails on invalid loan status (expired)", async function () {
+      const loanId = await createExpiredLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+      await expect(
+        pool.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(pool, "InvalidLoanStatus");
+    });
+    it("fails on invalid loan status (liquidated)", async function () {
+      const loanId = await createLiquidatedLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+      await expect(
+        pool.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(pool, "InvalidLoanStatus");
+    });
+    it("fails on unsupported loan duration", async function () {
+      const loanId = await createActiveLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"), 35 * 86400);
+      await expect(
+        pool.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(pool, "UnsupportedLoanDuration");
+    });
+    it("fails on unsupported collateral", async function () {
+      /* Create NFT 2 */
+      const testERC721Factory = await ethers.getContractFactory("TestERC721");
+      const nft2 = (await testERC721Factory.deploy("NFT 2", "NFT2", "https://nft2.com/token/")) as TestERC721;
+      await nft2.deployed();
+
+      /* Mint NFT 2 to borrower */
+      await nft2.mint(accountBorrower.address, 123);
+      /* Approve lending platform to transfer NFT */
+      await nft2.connect(accountBorrower).setApprovalForAll(lendingPlatform.address, true);
 
       /* Create loan */
       const lendTx = await lendingPlatform
         .connect(accountLender)
-        .lend(accountBorrower.address, nft1.address, 123, principal, repayment, 30 * 86400);
+        .lend(
+          accountBorrower.address,
+          nft2.address,
+          123,
+          ethers.utils.parseEther("25"),
+          ethers.utils.parseEther("26"),
+          30 * 86400
+        );
       const loanId = (await extractEvent(lendTx, lendingPlatform, "LoanCreated")).args.loanId;
 
-      /* Sell note */
-      await pool.connect(accountLender).sellNote(noteToken.address, loanId, principal, []);
-
-      /* FIXME decode and validate loan receipt */
-    });
-    it("fails on insufficient liquidity", async function () {
-      /* FIXME */
-    });
-    it("fails on unsupported collateral", async function () {
-      /* FIXME */
-    });
-    it("fails on invalid loan status", async function () {
-      /* FIXME */
+      await expect(
+        pool.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(pool, "UnsupportedCollateral", 0);
     });
     it("fails on unsupported currency token", async function () {
-      /* FIXME */
-    });
-    it("fails on unsupported loan duration", async function () {
-      /* FIXME */
+      /* Create Token 2 */
+      const testERC20Factory = await ethers.getContractFactory("TestERC20");
+      const tok2 = (await testERC20Factory.deploy(
+        "Token 2",
+        "TOK2",
+        18,
+        ethers.utils.parseEther("10000")
+      )) as TestERC20;
+      await tok2.deployed();
+
+      /* Deploy second pool */
+      const poolFactory = await ethers.getContractFactory("Pool", {
+        libraries: { LiquidityManager: liquidityManagerLib.address },
+      });
+      const pool2 = await poolFactory.deploy(
+        tok2.address,
+        30 * 86400,
+        allowCollateralFilter.address,
+        interestRateModel.address,
+        ethers.constants.AddressZero
+      );
+      await pool2.deployed();
+
+      /* Add note token to pool */
+      await pool2.setLoanAdapter(noteToken.address, noteAdapter.address);
+
+      /* Create loan */
+      const loanId = await createActiveLoan(ethers.utils.parseEther("25"), ethers.utils.parseEther("26"));
+
+      await expect(
+        pool2.sellNote(noteToken.address, loanId, ethers.utils.parseEther("25"), [])
+      ).to.be.revertedWithCustomError(pool, "UnsupportedCurrencyToken");
     });
   });
 });
