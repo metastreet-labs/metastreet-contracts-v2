617440484d22784e022d8f122d91ad13887fd838
Abdou Ouahib <ouahibdou@gmail.com>
2023-02-17 12:02:15 +0100

    subgraph: add CollateralToken entity

--- subgraph/src/poolFactory.ts
+++ subgraph/src/poolFactory.ts
@@ -1,18 +1,22 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { BigInt } from "@graphprotocol/graph-ts";
 import { ERC721 } from "../generated/PoolFactory/ERC721";
 import { ICollateralFilter } from "../generated/PoolFactory/ICollateralFilter";
 import { IPool } from "../generated/PoolFactory/IPool";
 import { PoolCreated } from "../generated/PoolFactory/IPoolFactory";
-import { Pool } from "../generated/schema";
+import { CollateralToken, Pool } from "../generated/schema";
 import { Pool as PoolTemplate } from "../generated/templates";
 
 export function handlePoolCreated(event: PoolCreated): void {
-  /* create pool entity */
-  const poolContract = IPool.bind(event.params.pool);
+  const poolAddress = event.params.pool;
+  const poolId = poolAddress.toHexString();
+  const poolContract = IPool.bind(poolAddress);
   const collateralFilterContract = ICollateralFilter.bind(poolContract.collateralFilter());
-  const poolEntity = new Pool(event.params.pool.toHexString());
+  const collateralTokenAddress = collateralFilterContract.tokens()[0];
+  const collateralTokenID = collateralTokenAddress.toHexString();
+  /* create pool entity */
+  const poolEntity = new Pool(poolId);
   poolEntity.currencyToken = poolContract.currencyToken();
-  poolEntity.maxLoanDuration = poolContract.maxLoanDuration();
+  poolEntity.maxLoanDuration = poolContract.maxLoanDuration().toI32();
   poolEntity.collateralFilter = poolContract.collateralFilter();
   poolEntity.interestRateModel = poolContract.interestRateModel();
   poolEntity.collateralLiquidator = poolContract.collateralLiquidator();
@@ -21,12 +25,33 @@ export function handlePoolCreated(event: PoolCreated): void {
   poolEntity.loansDefaultedCount = BigInt.zero();
   poolEntity.loansLiquidatedCount = BigInt.zero();
   poolEntity.totalValueLocked = BigInt.zero();
-  poolEntity.collateralToken = collateralFilterContract.tokens()[0];
-  const erc721Contract = ERC721.bind(Address.fromString(poolEntity.collateralToken.toHexString()));
-  const tokenName = erc721Contract.try_name();
-  if (tokenName.reverted) poolEntity.collateralTokenName = "Unnamed Token";
-  else poolEntity.collateralTokenName = tokenName.value;
+  poolEntity.maxBorrow = BigInt.zero();
+  poolEntity.collateralToken = collateralTokenID;
   poolEntity.save();
+
+  let collateralTokenEntity = CollateralToken.load(collateralTokenID);
+  if (collateralTokenEntity) {
+    /* Update collateral token entity if it exists */
+    const poolIds = collateralTokenEntity.poolIds;
+    poolIds.push(poolId);
+    collateralTokenEntity.poolIds = poolIds;
+    if (collateralTokenEntity.maxLoanDuration < poolEntity.maxLoanDuration) {
+      collateralTokenEntity.maxLoanDuration = poolEntity.maxLoanDuration;
+    }
+  } else {
+    /* Create collateral token entity if it doesn't exists */
+    collateralTokenEntity = new CollateralToken(collateralTokenID);
+    collateralTokenEntity.poolIds = [poolId];
+    collateralTokenEntity.totalValueLocked = BigInt.zero();
+    collateralTokenEntity.maxLoanDuration = poolEntity.maxLoanDuration;
+    collateralTokenEntity.minAPR = 0;
+    const erc721Contract = ERC721.bind(collateralTokenAddress);
+    const tokenName = erc721Contract.try_name();
+    if (tokenName.reverted) collateralTokenEntity.name = "Unnamed Token";
+    else collateralTokenEntity.name = tokenName.value;
+  }
+  collateralTokenEntity.save();
+
   /* create pool data source */
   PoolTemplate.create(event.params.pool);
 }
