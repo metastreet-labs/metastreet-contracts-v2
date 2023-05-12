7246404c7f2422e4af3eb06f54ec7f46cf5f1e62
Vanya A. Sergeev <v@sergeev.io>
2023-05-11 23:40:02 -0500

    hardhat: add mainnet network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,6 +23,10 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     goerli: {
       url: process.env.GOERLI_URL || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
