291bbcf6f2a7758baf9cc35c9b081c98fdfe29d3
wegory <gregoryteo@hotmail.sg>
2023-05-30 12:28:00 -0400

    contracts/interfaces: improve documentation of ICollateralLiquidationReceiver
    resolves cantinasec/review-metastreet#23

--- contracts/interfaces/ICollateralLiquidationReceiver.sol
+++ contracts/interfaces/ICollateralLiquidationReceiver.sol
@@ -7,6 +7,7 @@ pragma solidity 0.8.19;
 interface ICollateralLiquidationReceiver {
     /**
      * @notice Callback on collateral liquidated
+     * @dev Pre-conditions: 1) proceeds were transferred, and 2) transferred amount >= proceeds
      * @param liquidationContext Liquidation context
      * @param proceeds Liquidation proceeds in currency tokens
      */
