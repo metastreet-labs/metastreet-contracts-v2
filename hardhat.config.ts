361a321aefd4f17a249b0e5ef39ba1c66258c4d7
Vanya A. Sergeev <v@sergeev.io>
2023-06-13 23:15:40 -0500

    package: add hardhat-contract-sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-tracer";
+import "hardhat-contract-sizer";
 
 dotenv.config();
 
