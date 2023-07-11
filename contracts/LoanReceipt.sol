f62319b99c611a005fa5c8ad4055f38d48c77f65
maks-p <mpazuniak@gmail.com>
2023-07-11 13:10:09 -0400

    contracts: reuse error in LoanReceipt
    - Remove UnsupportedReceiptVersion

--- contracts/LoanReceipt.sol
+++ contracts/LoanReceipt.sol
@@ -17,11 +17,6 @@ library LoanReceipt {
      */
     error InvalidReceiptEncoding();
 
-    /**
-     * @notice Unsupported receipt version
-     */
-    error UnsupportedReceiptVersion();
-
     /**************************************************************************/
     /* Constants */
     /**************************************************************************/
@@ -182,7 +177,7 @@ library LoanReceipt {
         ) revert InvalidReceiptEncoding();
 
         /* Validate encoded receipt version */
-        if (uint8(encodedReceipt[0]) != LOAN_RECEIPT_V1_VERSION) revert UnsupportedReceiptVersion();
+        if (uint8(encodedReceipt[0]) != LOAN_RECEIPT_V1_VERSION) revert InvalidReceiptEncoding();
 
         LoanReceiptV1 memory receipt;
 
