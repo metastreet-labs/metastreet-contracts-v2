1617103ee1a4d7404b5b438f6a6b6406e50b4275
maks-p <mpazuniak@gmail.com>
2023-07-12 08:37:58 -0400

    contracts: remove variable in _optionDelegateCash

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -477,8 +477,13 @@ abstract contract Pool is
         if (delegateData.length != 0) {
             if (address(_delegationRegistry) == address(0) || delegateData.length != 20) revert InvalidBorrowOptions();
 
-            address delegate = address(uint160(bytes20(delegateData)));
-            _delegationRegistry.delegateForToken(delegate, collateralToken, collateralTokenId, true);
+            /* Delegate token */
+            _delegationRegistry.delegateForToken(
+                address(uint160(bytes20(delegateData))),
+                collateralToken,
+                collateralTokenId,
+                true
+            );
         }
     }
 
