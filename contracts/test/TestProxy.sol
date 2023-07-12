83717332732592ebb84fe471fd6b22524a8c4f81
maks-p <mpazuniak@gmail.com>
2023-07-12 08:35:41 -0400

    hardhat: update solidity compiler version to 0.8.20
    Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

--- contracts/test/TestProxy.sol
+++ contracts/test/TestProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
--- contracts/test/tokens/TestERC20.sol
+++ contracts/test/tokens/TestERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
