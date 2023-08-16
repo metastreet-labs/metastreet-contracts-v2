449869a9f51327fc2b4c8526221aafa0c493826e
Vanya A. Sergeev <v@sergeev.io>
2023-08-16 17:15:55 -0500

    test: rename multitoken ranged test to erc1155 ranged

--- test/Pool.erc1155.ranged.spec.ts
+++ test/Pool.erc1155.ranged.spec.ts
@@ -18,7 +18,7 @@ import { extractEvent, expectEvent } from "./helpers/EventUtilities";
 import { FixedPoint } from "./helpers/FixedPoint";
 import { Tick } from "./helpers/Tick";
 
-describe("Pool Batch Ranged Collection", function () {
+describe("Pool ERC1155 Ranged Collection", function () {
   let accounts: SignerWithAddress[];
   let tok1: TestERC20;
   let nft1: TestERC1155;
