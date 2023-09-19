8191122abff49e75c888c1ae75985f1a8e4af83c
wegory <gregoryteo@hotmail.sg>
2023-09-19 13:59:13 -0300

    contracts: fix missing natspec params for admin api in PoolFactory
    Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

--- contracts/PoolFactory.sol
+++ contracts/PoolFactory.sol
@@ -162,6 +162,7 @@ contract PoolFactory is Ownable, ERC1967Upgrade, IPoolFactory {
 
     /**
      * @notice Add pool implementation to allowlist
+     * @param poolImplementation Pool implementation
      */
     function addPoolImplementation(address poolImplementation) external onlyOwner {
         if (_allowedImplementations.add(poolImplementation)) {
@@ -171,6 +172,7 @@ contract PoolFactory is Ownable, ERC1967Upgrade, IPoolFactory {
 
     /**
      * @notice Remove pool implementation from allowlist
+     * @param poolImplementation Pool implementation
      */
     function removePoolImplementation(address poolImplementation) external onlyOwner {
         if (_allowedImplementations.remove(poolImplementation)) {
