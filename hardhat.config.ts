d436c856d483f53ea6229738d5f3913a1a994b31
Vanya A. Sergeev <v@sergeev.io>
2023-09-20 01:10:03 -0500

    hardhat: allow unlimited contract size on hardhat network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,6 +24,9 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
     mainnet: {
       url: process.env.MAINNET_URL || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
