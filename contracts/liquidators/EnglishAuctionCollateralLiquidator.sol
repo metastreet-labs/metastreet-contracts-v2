c0615fd57dd8d1138f69c1c9d63b2a2a6db3295d
wegory <gregoryteo@hotmail.sg>
2023-11-05 18:51:59 -0300

    contracts/liquidators: remove unused import in EnglishAuctionCL

--- contracts/liquidators/EnglishAuctionCollateralLiquidator.sol
+++ contracts/liquidators/EnglishAuctionCollateralLiquidator.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
