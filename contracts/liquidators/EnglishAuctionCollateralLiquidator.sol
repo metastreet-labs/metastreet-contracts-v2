8710299ae2b92e377fc5b1ff320de63649c054d4
maks-p <mpazuniak@gmail.com>
2023-11-13 14:50:43 -0500

    contracts/liquidators: remove unused import in EnglishAuctionCL
    resolves cantinasec/review-metastreet-november#4
    
    Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

--- contracts/liquidators/EnglishAuctionCollateralLiquidator.sol
+++ contracts/liquidators/EnglishAuctionCollateralLiquidator.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol"
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
