cd99f8fc4a322928ca20641321bba245506e4874
Vanya A. Sergeev <v@sergeev.io>
2023-11-30 01:52:58 -0600

    contracts/liquidators: update EnglishAuctionCL version to v2.0

--- contracts/liquidators/EnglishAuctionCollateralLiquidator.sol
+++ contracts/liquidators/EnglishAuctionCollateralLiquidator.sol
@@ -28,7 +28,7 @@ contract EnglishAuctionCollateralLiquidator is ICollateralLiquidator, Reentrancy
     /**
      * @notice Implementation version
      */
-    string public constant IMPLEMENTATION_VERSION = "1.2";
+    string public constant IMPLEMENTATION_VERSION = "2.0";
 
     /**
      * @notice Basis points scale
