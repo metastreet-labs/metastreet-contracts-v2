3910338f3ea77b661b1de40be37068eab7c45c25
Vanya A. Sergeev <v@sergeev.io>
2024-03-06 02:51:54 -0600

    contracts: update Pool version to v2.8

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -286,7 +286,7 @@ abstract contract Pool is
      * @return Implementation version
      */
     function IMPLEMENTATION_VERSION() external pure returns (string memory) {
-        return "2.7";
+        return "2.8";
     }
 
     /**
