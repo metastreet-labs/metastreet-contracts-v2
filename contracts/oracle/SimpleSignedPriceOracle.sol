9a88dbc9e5f3da053a18b44b36b7d9d05f23faca
wegory <gregoryteo@hotmail.sg>
2024-03-22 19:23:57 +0800

    contracts/oracle: add SimpleSignedPriceOracle
    Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

---
+++ contracts/oracle/SimpleSignedPriceOracle.sol
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+
+import "../interfaces/IPriceOracle.sol";
+
+/**
+ * @title Simple Signed Price Oracle
+ * @author MetaStreet Labs
+ */
+contract SimpleSignedPriceOracle is Ownable, EIP712, IPriceOracle {
+    /**************************************************************************/
+    /* Constants */
+    /**************************************************************************/
+
+    /**
+     * @notice Quote EIP-712 typehash
+     */
+    bytes32 public constant QUOTE_TYPEHASH =
+        keccak256(
+            "Quote(address token,uint256 tokenId,address currency,uint256 price,uint64 timestamp,uint64 duration)"
+        );
+
+    /**************************************************************************/
+    /* Errors */
+    /**************************************************************************/
+
+    /**
+     * @notice Invalid quote
+     */
+    error InvalidQuote();
+
+    /**
+     * @notice Invalid timestamp
+     */
+    error InvalidTimestamp();
+
+    /**
+     * @notice Invalid signature
+     */
+    error InvalidSignature();
+
+    /**
+     * @notice Invalid signer
+     */
+    error InvalidSigner();
+
+    /**
+     * @notice Invalid length
+     */
+    error InvalidLength();
+
+    /**************************************************************************/
+    /* Events */
+    /**************************************************************************/
+
+    /**
+     * @notice Emitted when price oracle signer is set
+     * @param collateralToken Collateral token
+     * @param signer Signer
+     */
+    event SignerUpdated(address indexed collateralToken, address signer);
+
+    /**************************************************************************/
+    /* Structures */
+    /**************************************************************************/
+
+    /**
+     * @notice Quote
+     * @param token Token
+     * @param tokenId Token ID
+     * @param currency Currency
+     * @param price Price
+     * @param timestamp Timestamp
+     * @param duration Duration validity
+     */
+    struct Quote {
+        address token;
+        uint256 tokenId;
+        address currency;
+        uint256 price;
+        uint64 timestamp;
+        uint64 duration;
+    }
+
+    /**
+     * @notice Quote with signature
+     * @param quote Quote
+     * @param signature ECDSA signature
+     */
+    struct SignedQuote {
+        Quote quote;
+        bytes signature;
+    }
+
+    /**************************************************************************/
+    /* State */
+    /**************************************************************************/
+
+    /**
+     * @notice Mapping of collection to price oracle signers
+     */
+    mapping(address => address) _priceOracleSigners;
+
+    /**************************************************************************/
+    /* Constructor */
+    /**************************************************************************/
+
+    /**
+     * @notice Simple Signed Price Oracle constructor
+     * @param name_ Domain separator name
+     */
+    constructor(string memory name_) EIP712(name_, IMPLEMENTATION_VERSION()) {}
+
+    /**************************************************************************/
+    /* Internal Helpers */
+    /**************************************************************************/
+
+    /**
+     * @notice Verify quote and signer
+     * @param collateralToken Collateral token
+     * @param collateralTokenId Collateral token ID
+     * @param poolCurrency Pool currency
+     * @param signedQuote Signed quote
+     */
+    function _verifyQuote(
+        address collateralToken,
+        uint256 collateralTokenId,
+        address poolCurrency,
+        SignedQuote memory signedQuote
+    ) internal view {
+        Quote memory quote = signedQuote.quote;
+
+        /* Validate quote token, token ID, and currency */
+        if (collateralToken != quote.token || collateralTokenId != quote.tokenId || poolCurrency != quote.currency)
+            revert InvalidQuote();
+
+        /* Validate quote timestamp */
+        if (quote.timestamp > block.timestamp || quote.timestamp + quote.duration < block.timestamp)
+            revert InvalidTimestamp();
+
+        /* Recover quote signer */
+        address signerAddress = ECDSA.recover(
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        QUOTE_TYPEHASH,
+                        collateralToken,
+                        collateralTokenId,
+                        poolCurrency,
+                        quote.price,
+                        quote.timestamp,
+                        quote.duration
+                    )
+                )
+            ),
+            signedQuote.signature
+        );
+
+        /* Validate signer */
+        if (signerAddress != _priceOracleSigners[collateralToken]) revert InvalidSigner();
+    }
+
+    /**************************************************************************/
+    /* Getters */
+    /**************************************************************************/
+
+    /**
+     * @notice Get price oracle implementation version
+     * @return Price oracle implementation version
+     */
+    function IMPLEMENTATION_VERSION() public pure returns (string memory) {
+        return "1.0";
+    }
+
+    /**
+     * @notice Get price oracle signer for collateral token
+     * @param collateralToken Collateral token
+     * @return Price oracle signer
+     */
+    function priceOracleSigner(address collateralToken) external view returns (address) {
+        return _priceOracleSigners[collateralToken];
+    }
+
+    /**************************************************************************/
+    /* API */
+    /**************************************************************************/
+
+    /**
+     * @inheritdoc IPriceOracle
+     */
+    function price(
+        address collateralToken,
+        address currencyToken,
+        uint256[] memory collateralTokenIds,
+        uint256[] memory collateralTokenQuantities,
+        bytes calldata oracleContext
+    ) external view override returns (uint256) {
+        /* Decode oracle context into a SignedQuote array */
+        SignedQuote[] memory signedQuotes = abi.decode(oracleContext, ((SignedQuote)[]));
+
+        /* Validate arrays have equal lengths */
+        if (signedQuotes.length != collateralTokenIds.length) revert InvalidLength();
+        if (collateralTokenIds.length != collateralTokenQuantities.length) revert InvalidLength();
+
+        /* Validate and aggregate oracle prices */
+        uint256 totalOraclePrice;
+        uint256 count;
+        for (uint256 i; i < collateralTokenIds.length; i++) {
+            /* Validate quote and signer */
+            _verifyQuote(collateralToken, collateralTokenIds[i], currencyToken, signedQuotes[i]);
+
+            /* Update total oracle price and collateral token count */
+            totalOraclePrice += signedQuotes[i].quote.price * collateralTokenQuantities[i];
+            count += collateralTokenQuantities[i];
+        }
+
+        /* Return average collateral token price */
+        return totalOraclePrice / count;
+    }
+
+    /**************************************************************************/
+    /* Admin API */
+    /**************************************************************************/
+
+    /**
+     * @notice Set price oracle signer for collateral token
+     *
+     * Emits a {SignerUpdated} event.
+     *
+     * @param collateralToken Collateral token
+     * @param signer Signer
+     */
+    function setSigner(address collateralToken, address signer) external onlyOwner {
+        _priceOracleSigners[collateralToken] = signer;
+
+        emit SignerUpdated(collateralToken, signer);
+    }
+}
---
+++ test/oracles/SimpleSignedPriceOracle.spec.ts
@@ -0,0 +1,312 @@
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+
+import * as helpers from "@nomicfoundation/hardhat-network-helpers";
+
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+import { SimpleSignedPriceOracle } from "../../typechain";
+
+describe("SimpleSignedPriceOracle", function () {
+  let simpleSignedPriceOracle: SimpleSignedPriceOracle;
+  let snapshotId: string;
+  let accounts: SignerWithAddress[];
+
+  /* Constants */
+  const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const DOODLES_ADDRESS = "0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e";
+  const DOODLES_ID = 1;
+  const WPUNKS_ADDRESS = "0xb7F7F6C52F2e2fdb1963Eab30438024864c313F6";
+  const WPUNK_ID_1 = 6887;
+  const WPUNK_ID_2 = 6888;
+
+  before("deploy fixture", async function () {
+    accounts = await ethers.getSigners();
+
+    const simpleSignedPriceOracleFactory = await ethers.getContractFactory("SimpleSignedPriceOracle");
+
+    simpleSignedPriceOracle = (await simpleSignedPriceOracleFactory.deploy("testName")) as SimpleSignedPriceOracle;
+    simpleSignedPriceOracle.deployed();
+  });
+
+  beforeEach("snapshot blockchain", async () => {
+    snapshotId = await network.provider.send("evm_snapshot", []);
+  });
+
+  afterEach("restore blockchain snapshot", async () => {
+    await network.provider.send("evm_revert", [snapshotId]);
+  });
+
+  /****************************************************************************/
+  /* Constants */
+  /****************************************************************************/
+
+  describe("constants", async function () {
+    it("matches price oracle implementation version", async function () {
+      expect(await simpleSignedPriceOracle.IMPLEMENTATION_VERSION()).to.be.equal("1.0");
+    });
+  });
+
+  /****************************************************************************/
+  /* Helper functions */
+  /****************************************************************************/
+
+  const QUOTE_TYPEHASH = {
+    Quote: [
+      { name: "token", type: "address" },
+      { name: "tokenId", type: "uint256" },
+      { name: "currency", type: "address" },
+      { name: "price", type: "uint256" },
+      { name: "timestamp", type: "uint64" },
+      { name: "duration", type: "uint64" },
+    ],
+  };
+
+  async function createSignedQuote(
+    signer: SignerWithAddress,
+    token: string,
+    tokenId: ethers.BigNumber,
+    price: ethers.BigNumber,
+    currency?: string = WETH_ADDRESS
+  ) {
+    const DOMAIN = {
+      name: "testName" /* TBD */,
+      version: "1.0" /* TBD */,
+      chainId: 1,
+      verifyingContract: simpleSignedPriceOracle.address,
+    };
+
+    /* Time now */
+    const timestamp = await helpers.time.latest();
+
+    /* 5 minutes */
+    const duration = 60 * 5;
+
+    let quote = {
+      token,
+      tokenId,
+      currency,
+      price,
+      timestamp,
+      duration,
+    };
+
+    const signature = await signer._signTypedData(DOMAIN, QUOTE_TYPEHASH, quote);
+
+    return [[token, tokenId, currency, price, timestamp, duration], signature];
+  }
+
+  /****************************************************************************/
+  /* Admin API */
+  /****************************************************************************/
+
+  describe("#setSigner", async function () {
+    it("set signer successfully", async function () {
+      await simpleSignedPriceOracle.setSigner(WPUNKS_ADDRESS, accounts[0].address);
+      await simpleSignedPriceOracle.setSigner(DOODLES_ADDRESS, accounts[1].address);
+
+      expect(await simpleSignedPriceOracle.priceOracleSigner(WPUNKS_ADDRESS)).to.be.equal(accounts[0].address);
+      expect(await simpleSignedPriceOracle.priceOracleSigner(DOODLES_ADDRESS)).to.be.equal(accounts[1].address);
+
+      await simpleSignedPriceOracle.setSigner(WPUNKS_ADDRESS, ethers.constants.AddressZero);
+      await simpleSignedPriceOracle.setSigner(DOODLES_ADDRESS, ethers.constants.AddressZero);
+
+      expect(await simpleSignedPriceOracle.priceOracleSigner(WPUNKS_ADDRESS)).to.be.equal(ethers.constants.AddressZero);
+      expect(await simpleSignedPriceOracle.priceOracleSigner(DOODLES_ADDRESS)).to.be.equal(
+        ethers.constants.AddressZero
+      );
+    });
+
+    it("fails on non-owner setting signer", async function () {
+      await expect(
+        simpleSignedPriceOracle.connect(accounts[1]).setSigner(WPUNKS_ADDRESS, accounts[0].address)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+  });
+
+  /****************************************************************************/
+  /* Primary API */
+  /****************************************************************************/
+
+  describe("#price", async function () {
+    beforeEach("set signer for WPUNKs", async function () {
+      await simpleSignedPriceOracle.setSigner(WPUNKS_ADDRESS, accounts[0].address);
+    });
+
+    it("successfully return price", async function () {
+      const message_1 = await createSignedQuote(accounts[0], WPUNKS_ADDRESS, WPUNK_ID_1, ethers.utils.parseEther("2"));
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message_1]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate for 1 collateral token ID */
+      expect(
+        await simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [1], oracleContext)
+      ).to.be.equal(ethers.utils.parseEther("2"));
+
+      const message_2 = await createSignedQuote(accounts[0], WPUNKS_ADDRESS, WPUNK_ID_2, ethers.utils.parseEther("4"));
+      oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message_1, message_2]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate for 2 collateral token IDs */
+      expect(
+        await simpleSignedPriceOracle.price(
+          WPUNKS_ADDRESS,
+          WETH_ADDRESS,
+          [WPUNK_ID_1, WPUNK_ID_2],
+          [1, 1],
+          oracleContext
+        )
+      ).to.be.equal(ethers.utils.parseEther("3"));
+
+      /* Validate for 2 collateral token IDs with quantity 2 and 3 respectively */
+      expect(
+        await simpleSignedPriceOracle.price(
+          WPUNKS_ADDRESS,
+          WETH_ADDRESS,
+          [WPUNK_ID_1, WPUNK_ID_2],
+          [2, 3],
+          oracleContext
+        )
+      ).to.be.equal(ethers.utils.parseEther("3.2"));
+    });
+
+    it("fails on invalid token", async function () {
+      const message = await createSignedQuote(
+        accounts[0],
+        DOODLES_ADDRESS,
+        WPUNK_ID_1,
+        ethers.utils.parseEther("2"),
+        USDC_ADDRESS
+      );
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate for invalid token */
+      await expect(
+        simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [1], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidQuote");
+    });
+
+    it("fails on invalid token ID", async function () {
+      const message = await createSignedQuote(
+        accounts[0],
+        WPUNKS_ADDRESS,
+        WPUNK_ID_2,
+        ethers.utils.parseEther("2"),
+        USDC_ADDRESS
+      );
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate for invalid token ID */
+      await expect(
+        simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [1], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidQuote");
+    });
+
+    it("fails on invalid currency", async function () {
+      const message = await createSignedQuote(
+        accounts[0],
+        WPUNKS_ADDRESS,
+        WPUNK_ID_1,
+        ethers.utils.parseEther("2"),
+        USDC_ADDRESS
+      );
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate for invalid currency */
+      await expect(
+        simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [1], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidQuote");
+    });
+
+    it("fails on invalid timestamp", async function () {
+      const message = await createSignedQuote(accounts[0], WPUNKS_ADDRESS, WPUNK_ID_1, ethers.utils.parseEther("2"));
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message]]
+      );
+
+      /* Fast forward 6 minutes */
+      await helpers.time.increase(360);
+
+      /* Validate invalid timestamp (expired) */
+      await expect(
+        simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [1], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidTimestamp");
+    });
+
+    it("fails on invalid signer", async function () {
+      const message_1 = await createSignedQuote(accounts[1], WPUNKS_ADDRESS, WPUNK_ID_1, ethers.utils.parseEther("2"));
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message_1]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate invalid signature */
+      await expect(
+        simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [1], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidSigner");
+
+      const message_2 = await createSignedQuote(accounts[0], DOODLES_ADDRESS, DOODLES_ID, ethers.utils.parseEther("2"));
+      oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message_2]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate invalid signature */
+      await expect(
+        simpleSignedPriceOracle.price(DOODLES_ADDRESS, WETH_ADDRESS, [DOODLES_ID], [1], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidSigner");
+    });
+
+    it("fails on invalid length", async function () {
+      const message = await createSignedQuote(accounts[0], WPUNKS_ADDRESS, WPUNK_ID_1, ethers.utils.parseEther("2"));
+      let oracleContext = ethers.utils.defaultAbiCoder.encode(
+        ["((address,uint256,address,uint256,uint64,uint64),bytes)[]"],
+        [[message]]
+      );
+
+      /* Fast forward 30 seconds */
+      await helpers.time.increase(30);
+
+      /* Validate invalid length */
+      await expect(
+        simpleSignedPriceOracle.price(WPUNKS_ADDRESS, WETH_ADDRESS, [WPUNK_ID_1], [], oracleContext)
+      ).to.be.revertedWithCustomError(simpleSignedPriceOracle, "InvalidLength");
+    });
+  });
+});
