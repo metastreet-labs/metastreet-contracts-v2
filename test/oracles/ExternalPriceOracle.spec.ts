719766780f271b7df98a6a778292d5b9a64cce9b
wegory <gregoryteo@hotmail.sg>
2024-04-16 10:41:17 +0800

    contracts/oracle: make price() api public in PriceOracle
    Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

--- test/oracles/ExternalPriceOracle.spec.ts
+++ test/oracles/ExternalPriceOracle.spec.ts
@@ -52,7 +52,16 @@ describe("ExternalPriceOracle", function () {
 
   describe("#price", async function () {
     it("returns 0 if price oracle address is 0", async function () {
-      expect(await externalPriceOracle2.price(COLLATERAL_TOKEN, CURRENCY_TOKEN, [], [], "0x11", "0x")).to.be.equal(0);
+      expect(
+        await externalPriceOracle2["price(address,address,uint256[],uint256[],bytes,bytes)"](
+          COLLATERAL_TOKEN,
+          CURRENCY_TOKEN,
+          [],
+          [],
+          "0x11",
+          "0x"
+        )
+      ).to.be.equal(0);
     });
   });
 });
