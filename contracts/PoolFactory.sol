5e469b9d081bec360ffd06b13002b244770aed42
wegory <gregoryteo@hotmail.sg>
2024-07-12 14:51:27 +0800

    contracts: add withdrawAdminFees() api to PoolFactory
    Signed-off-by: Vanya A. Sergeev <v@sergeev.io>

--- contracts/PoolFactory.sol
+++ contracts/PoolFactory.sol
@@ -183,6 +183,18 @@ contract PoolFactory is Ownable, ERC1967Upgrade, IPoolFactory {
     }
 
     /**
+     * @notice Withdraw admin fees
+     * @param pool Pool address
+     * @param recipient Recipient of admin fees (less fee share)
+     */
+    function withdrawAdminFees(address pool, address recipient) external onlyOwner {
+        /* Validate pool */
+        if (!isPool(pool)) revert InvalidPool();
+
+        Address.functionCall(pool, abi.encodeWithSignature("withdrawAdminFees(address)", recipient));
+    }
+
+    /**
      * @notice Add pool implementation to allowlist
      * @param poolImplementation Pool implementation
      */
