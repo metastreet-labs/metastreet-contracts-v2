70236312909647cd8874708f674340740a059430
Greg T. <greg@permianlabs.xyz>
2025-06-17 15:11:34 +0700

    hardhat: increase optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,7 +40,7 @@ const config: HardhatUserConfig = {
             viaIR: true,
             optimizer: {
               enabled: true,
-              runs: 100,
+              runs: 149,
             },
             debug: {
               revertStrings: "strip",
